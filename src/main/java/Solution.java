583827eeeca977dc955aa9266543198eb6de5ffd
ron <ron.lobo@yahoo.com>
2022-05-14 14:15:15 +0100

    033: solution update

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,19 +4,22 @@ class Solution {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        System.out.println(in.nextLine().chars().filter(Character::isAlphabetic).filter(c -> c % 2 == 0).count());
 
-        System.out.println(in.nextLine().chars().filter(c -> c % 2 == 0 && Character.isAlphabetic(c)).count());
+        String s = "AAAABBBCCDEFFG"; // in.nextLine();
+        System.out.println(s.chars().mapToObj(Character::toString).reduce((a, b) -> a + (b.equals(a.substring(a.length() - 1)) ? b : " " + b)));
 
-        System.out.println(in.nextLine().chars().filter(c -> c % 2 == 0 && ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))).count());
 
-        // String m = in.nextLine();
-        // int sum = 0;
-        // for (char a : m.toCharArray()) {
-        //     if (Character.isAlphabetic(a) && a%2==0) {
-        //         sum += 1;
-        //     }
-        // }
-        // System.out.println(sum);
+        s = "The quick brown fox"; // in.nextLine();
+        System.out.println(s.chars().filter(Character::isAlphabetic).filter(c -> c % 2 == 0).count());
+        System.out.println(s.chars().filter(c -> c % 2 == 0 && Character.isAlphabetic(c)).count());
+        System.out.println(s.chars().filter(c -> c % 2 == 0 && ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))).count());
+
+        int sum = 0;
+        for (char a : s.toCharArray()) {
+            if (Character.isAlphabetic(a) && a % 2 == 0) {
+                sum += 1;
+            }
+        }
+        System.out.println(sum);
     }
 }
