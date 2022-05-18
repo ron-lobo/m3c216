e874ec604adec57991e438450bb4c3822fd64233
ron <ron.lobo@yahoo.com>
2022-05-18 14:48:55 +0100

    034: mockito-all -> mockito-core

--- src/test/java/com/m3/dvdlib/DvdLibControllerTest.java
+++ src/test/java/com/m3/dvdlib/DvdLibControllerTest.java
@@ -3,6 +3,7 @@ package com.m3.dvdlib;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -18,7 +19,7 @@ class DvdLibControllerTest {
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
 
     @AfterEach
@@ -40,13 +41,13 @@ class DvdLibControllerTest {
     @Test
     void runListDvds() {
         System.out.println("view = " + view);
-        when(view.mainMenu()).thenReturn(5).thenReturn(7).thenThrow(new Exception("help"));
+        when(view.mainMenu()).thenReturn(5).thenReturn(7).thenThrow(new RuntimeException("help"));
 
         DvdLibController dvdLibController = new DvdLibController(view, dao);
         dvdLibController.run();
 
         verify(view).goodbyeMessage();
-        verify(view).dvdList(anyListOf(Dvd.class));
+        verify(view).dvdList(ArgumentMatchers.anyList());
         verify(dao).listAllDvds();
     }
 }
